d55763234cd3e2238092cc82c19c246de6b316da
haonp <haonguyenprc@gmail.com>
2026-01-29 11:10:00 +0800

    Connect to PostgreSQL database and update static file serving settings

--- my_site/settings.py
+++ my_site/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,13 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rm(4_rn0jb$q$6kep^8^i&$_+tdnv4ua0#xwo!o9u^(=0@glr#"
+SECRET_KEY = getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = getenv("IS_DEVELOPMENT", True)
 
 ALLOWED_HOSTS = [
-    getenv("APP_HOST")
+    getenv("APP_HOST", "127.0.0.1"),
+    "arcade.dpdns.org",
+    "www.arcade.dpdns.org"
 ]
 
 
@@ -80,8 +85,12 @@ WSGI_APPLICATION = "my_site.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": getenv("DB_USER"),
+        "PASSWORD": getenv("DB_PASSWORD"),
+        "HOST": getenv("DB_HOST"),
+        "PORT": getenv("DB_PORT")
     }
 }
 
--- my_site/urls.py
+++ my_site/urls.py
@@ -23,4 +23,4 @@ from django.conf import settings
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("blog.urls"))
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
